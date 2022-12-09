37a59b27f27f5e239d59d4ab131ee0b618071b42
vishal kumar <118764561+vishalkeshri123@users.noreply.github.com>
2022-12-09 14:55:11 +0530

    Update project123.py

--- project123.py
+++ project123.py
@@ -4,10 +4,10 @@ if(cakeangle%N==0):
     print("YES the cake will cut in equal pieces of size %d"%N)
 else:
     print("NO the cake will not cut in equal pieces of size %d"%N)
-if(N>cakeangle):#only when N is greater the cake angle cake can't be cut into pieces
+if(N>cakeangle):
     print("NO the cake will not cut in any pieces of size %d"%N)
 else:
-    print("YES the cake will not cut in any pieces of size %d"%N)
+    print("YES cake will not cut in any pieces of size %d"%N)
 n=1 #start substracting the cake 
 for i in range(N):
     cakeangle=n
